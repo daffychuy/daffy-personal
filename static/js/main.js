5fba3b51d337b2f0aa6dd505857bb89682a6d75f
Daniel Chu <daffy@daffy.local>
2019-01-01 14:34:00 -0500

    Update main index file to latest

---
+++ static/js/main.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function($) {
+    /* ======= Fixed header when scrolled ======= */
+
+    $(window).bind('scroll', function() {
+         if ($(window).scrollTop() > 50) {
+             $('#header').addClass('navbar-fixed-top');
+         }
+         else {
+             $('#header').removeClass('navbar-fixed-top');
+         }
+    });
+
+});
