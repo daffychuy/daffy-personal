4bbaf474748cf3a97a37b14ad3bf8b9220aa04b7
Daffy Chu <danielchu0710@hotmail.com>
2020-04-26 01:32:13 -0400

    Attempt to fix loading time on remote

--- src/app/core/services/home.service.ts
+++ src/app/core/services/home.service.ts
@@ -2,6 +2,11 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { home } from './home.interface';
+import { delay } from "rxjs/operators";
+import { InjectionToken, FactoryProvider } from '@angular/core';
+
+
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -13,8 +18,7 @@ export class HomeService {
   ) { }
 
   getAll(): Observable<home[]> {
-
-    return this.http.get<home[]>('/api/personal/v1/personal');
+    return this.http.get<home[]>('/api/personal/v1/personal').pipe(delay(0));
   }
 
 }
